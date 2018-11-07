1f91ba37b508c55d45b03e47f54b9261e5b5ae2d
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:19:02 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

---
+++ software/examples/rust/example_simple.rs
@@ -0,0 +1,24 @@
+use std::{error::Error, io};
+
+use tinkerforge::{analog_out_v3_bricklet::*, ip_connection::IpConnection};
+
+const HOST: &str = "localhost";
+const PORT: u16 = 4223;
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Analog Out Bricklet 3.0.
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let ao = AnalogOutV3Bricklet::new(UID, &ipcon); // Create device object.
+
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
+
+    // Set output voltage to 3.3V
+    ao.set_output_voltage(3300);
+
+    println!("Press enter to exit.");
+    let mut _input = String::new();
+    io::stdin().read_line(&mut _input)?;
+    ipcon.disconnect();
+    Ok(())
+}
