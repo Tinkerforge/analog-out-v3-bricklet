a39289d73f60039245b32d3e04423f679bed3ca2
Erik Fleckstein <erik@tinkerforge.com>
2020-01-13 17:14:54 +0100

    examples: Use python3 input instead of python2 raw_input.

--- software/examples/python/example_simple.py
+++ software/examples/python/example_simple.py
@@ -18,5 +18,5 @@ if __name__ == "__main__":
     # Set output voltage to 3.3V
     ao.set_output_voltage(3300)
 
-    raw_input("Press key to exit\n") # Use input() in Python 3
+    input("Press key to exit\n") # Use raw_input() in Python 2
     ipcon.disconnect()
